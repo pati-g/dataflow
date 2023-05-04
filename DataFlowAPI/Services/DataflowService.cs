0b9e4503b806fe70560e8adc1e8947237ebb3587
Patrycja Galant <patrycja.ciruk@gmail.com>
2023-05-04 09:08:08 +0200

    Add a condition that checks if a selected dataframe has the key column

--- DataFlowAPI/Services/DataflowService.cs
+++ DataFlowAPI/Services/DataflowService.cs
@@ -52,7 +52,10 @@ namespace DataFlowAPI.Services
                 for (int i = 1; i < dfList.Count; i++)
                 {
                     var df = dfList[i];
-                    dfMerged = dfMerged.Merge(df, new string[] { key.Name }, new string[] { key.Name });
+                    if (df.Columns.Select(c => c.Name).Contains(key.Name))
+                    {
+                        dfMerged = dfMerged.Merge(df, new string[] { key.Name }, new string[] { key.Name });
+                    }
                 }
             }
             return dfMerged;
